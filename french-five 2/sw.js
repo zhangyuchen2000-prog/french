0506b5786c7c72d202e7a16b33a462deeac82b59
zhangyuchen2000-prog <zhangyuchen2000@gmail.com>
2026-07-13 21:56:39 +1200

    Delete french-five 2 directory

--- french-five 2/sw.js
+++
@@ -1,6 +0,0 @@
-const CACHE='cinq-v2';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
-
-self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
